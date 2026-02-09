50f2ea4ee246e5a7502a262defc87af4cf1992a8
Diogo Silva <diogoalexsilva123@gmail.com>
2026-02-09 00:17:45 -0300

    fix: login redirect loop

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,14 @@ from core.views import home
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("login/", auth_views.LoginView.as_view(template_name="auth/login.html"), name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="auth/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
     path("", home, name="home"),
