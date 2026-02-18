53167ba003a5903008a0cff179293e3561ea2391
Diogo Silva <diogoalexsilva123@gmail.com>
2026-02-18 00:01:04 -0300

    adcionado o icone de carrinho com bagdeg de numero de item colocado o carrinho oficial versão instavel

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,19 +1,23 @@
-from .models import TransferOrder, OrderStatus
+from core.models import TransferOrder, OrderStatus
 
 def cart_badge(request):
     if not request.user.is_authenticated:
         return {}
 
-    cart = TransferOrder.objects.filter(
-        created_by=request.user,
-        status=OrderStatus.DRAFT
-    ).first()
+    if request.user.groups.filter(name="QUEIMADOS").exists():
 
-    count = 0
-    if cart:
-        count = cart.items.count()
+        cart = TransferOrder.objects.filter(
+            created_by=request.user,
+            status=OrderStatus.DRAFT
+        ).first()
 
-    return {
-        "cart_count": count
-    }
+        if cart:
+            total = sum(item.qty_requested for item in cart.items.all())
+        else:
+            total = 0
 
+        return {
+            "cart_count": total
+        }
+
+    return {}
