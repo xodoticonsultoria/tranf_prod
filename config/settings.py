ce4c5560d0e1f8d571f5b28f958ab3ab8fe26145
Diogo Silva <diogoalexsilva123@gmail.com>
2026-02-18 23:24:27 -0300

    fluxs em temop real com redis oficial versão instavel 02

--- config/settings.py
+++ config/settings.py
@@ -149,9 +149,17 @@ USE_TZ = True
 # STATIC + MEDIA
 # ======================
 
+# ======================
+# STATIC
+# ======================
+
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# WhiteNoise storage correto para produção
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
