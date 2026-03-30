52bec6a97c4d7df3bc3268e4df296ad73b7518dd
DIOOWS <dioowdioowtrd@gmail.com>
2026-03-29 22:42:51 -0300

    fix: adicionando css c

--- config/settings.py
+++ config/settings.py
@@ -164,9 +164,9 @@ USE_TZ = True
 # ======================
 
 STATIC_URL = "/static/"
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # 🔥 ESSENCIAL PRA PRODUÇÃO
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
