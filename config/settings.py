0227a2dc06089793fca59d13338039f0e6b11b84
DIOOWS <dioowdioowtrd@gmail.com>
2026-04-13 23:29:17 -0300

    CORRIGINDO O STATICS

--- config/settings.py
+++ config/settings.py
@@ -151,8 +151,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # ======================
 # MEDIA (CLOUDINARY)
